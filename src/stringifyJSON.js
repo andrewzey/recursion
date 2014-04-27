e7604dbdde14a9d43fbea2909a639a8d944245b6
Andrew Zey <andrew@andrewzey.com>
2014-04-27 11:12:59 -0700

    stringifyJSON - made first 6 test cases pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,33 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+
+  //if obj is a number
+  if (typeof obj === "number") {
+  	result = obj.toString();
+  }
+
+  //if obj is undefined
+  if (!obj) {
+  	result = 'null';
+  }
+
+  //if obj is boolean
+  if (typeof obj === "boolean") {
+  	result = (obj) ? 'true' : 'false';
+  }
+
+  //if obj is a string
+  if (typeof obj === "string") {
+  	result += '"' + obj + '"';
+  }
+
+  //if obj is array
+  if (Array.isArray(obj)) {
+  	result = (obj.length === 0) ? '[]': '';
+  }
+  
+  console.log(result);
+  return result;
 };
